a7b5a14be58b68cf08f9f808ff1e2c176297ad79
chriskohlbrenner <c.kohlbrenner@gmail.com>
2014-05-28 12:54:37 -0400

    adjust dependencies

--- app.js
+++ app.js
@@ -1,19 +1,16 @@
+
+/**
+ * Module dependencies.
+ */
+
 var express = require('express');
 var routes = require('./routes');
 var user = require('./routes/user');
-var http = require('http');var path = require('path');
-var favicon = require('static-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-
-
-
+var http = require('http');
+var path = require('path');
 
 var app = express();
 
-// Chat setup
 var sockjs = require('sockjs');
 
 var connections = [];
@@ -35,57 +32,27 @@ chat.on('connection', function(conn) {
     });
 });
 
-
-// view engine setup
+// all environments
+app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
-app.use(favicon());
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use(cookieParser());
-app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/user', user);
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+app.get('/users', user.list);
 
 var server = http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
-chat.installHandlers(server, {prefix:'/chat/info'});
-
-/// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-/// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
-
-
-module.exports = app;
+chat.installHandlers(server, {prefix:'/chat'});
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
-var express = require('express');
-var router = express.Router();
+// var express = require('express');
+// var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'ExpressChat' });
-});
+// /* GET home page. */
+// router.get('/', function(req, res) {
+//   res.render('index', { title: 'ExpressChat' });
+// });
 
-module.exports = router;
+// module.exports = router;
+
+exports.index = function(req, res){
+  res.render('index', { title: 'Express' });
+};
