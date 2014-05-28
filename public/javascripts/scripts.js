b56b230f18246f48ece7a8c623415c086a895dd7
chriskohlbrenner <c.kohlbrenner@gmail.com>
2014-05-28 14:54:18 -0400

    finish scrolling fix

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,7 +1,7 @@
+// JQuery to scroll down when messages reach bottom of page
 $(document).ready(function() {
-  
   $("form").on("submit", function() {
-    $("html, body").animate({ scrollTop: $(document).height() }, "slow");
+    $("html, body").animate({ scrollTop: $(document).height() }, "fast");
     return false;
   });
 });
